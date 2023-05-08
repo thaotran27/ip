e924248c491ebd04286715c06472860e89a2853b
Thao <tranthaoptcnn@gmail.com>
2023-05-08 15:31:02 +0100

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +6,30 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String greetings = "Hello! I'm Duke\n"
+                + "What can I do for you?";
+        String horizontal_line = "_".repeat(20);
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(horizontal_line);
+        System.out.println(greetings);
+        System.out.println(horizontal_line);
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String prompt = scanner.nextLine();  // Read user input
+            if(prompt.equals("bye")) {
+                System.out.println(horizontal_line);
+                System.out.println("Bye. Hope to see you again soon");
+                System.out.println(horizontal_line);
+                return;
+            } else {
+                System.out.println(horizontal_line);
+                System.out.println(prompt);
+                System.out.println(horizontal_line);
+
+            }
+
+        }
+
     }
 }
