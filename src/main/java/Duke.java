d4fe530574f45af791cb69f5e0b4d9c8ce1a4003
Thao <tranthaoptcnn@gmail.com>
2023-05-08 16:52:56 +0100

    level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,25 @@
 import java.util.*;
+class Task {
+
+    String name;
+    boolean isDone;
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public boolean getIsDone() {
+        return isDone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setIsDone(boolean newIsDone) {
+        this.isDone = newIsDone;
+    }
+}
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -16,9 +37,10 @@ public class Duke {
         System.out.println(horizontal_line);
         Scanner scanner = new Scanner(System.in);
 
-        List<String> taskList = new ArrayList<String>();
+        List<Task> taskList = new ArrayList<Task>();
         while(true) {
             String prompt = scanner.nextLine();  // Read user input
+            String[] arr = prompt.split(" ");
             if(prompt.equals("bye")) {
                 System.out.println(horizontal_line);
                 System.out.println("Bye. Hope to see you again soon");
@@ -27,13 +49,24 @@ public class Duke {
             } else if (prompt.equals("list")) {
                 System.out.println(horizontal_line);
                 int counter = 1;
-                for(String task : taskList) {
-                    System.out.printf("%d. %s\n", counter, task);
+                for (Task task : taskList) {
+                    String taskState = "✗";
+                    if (task.getIsDone()){
+                        taskState = "✓";
+                    }
+                    System.out.printf("%d.[%s] %s\n", counter,taskState, task.getName());
                     counter++;
                 }
                 System.out.println(horizontal_line);
+            } else if (arr[0].equals("done")) {
+                int taskNumber = Integer.parseInt(arr[1]);
+                taskList.get(taskNumber-1).setIsDone(true);
+                System.out.println(horizontal_line);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.printf("[✓] %s\n", taskList.get(taskNumber-1).getName());
+                System.out.println(horizontal_line);
             } else {
-                taskList.add(prompt);
+                taskList.add(new Task(prompt));
                 System.out.println(horizontal_line);
                 System.out.printf("added: %s\n", prompt);
                 System.out.println(horizontal_line);
